8f95073de4575ee914d499c0b1f70c05c48e8f3d
Wojciech Masicz <wmasicz@gmail.com>
2020-04-03 16:54:30 +0200

    Updates

---
+++ precache-manifest.df192025babde86cc2689be6ef5effe2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "895ff1eb5d9f3eb526734e34f96a8d3b",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "b6b95daab0be455cd3ef",
+    "url": "/calculator/static/css/main.9c76bc4d.chunk.css"
+  },
+  {
+    "revision": "bab23a6bcaabdc2c56ab",
+    "url": "/calculator/static/js/2.7656a563.chunk.js"
+  },
+  {
+    "revision": "01b52e8cfa161811ba4cbd78f485a21f",
+    "url": "/calculator/static/js/2.7656a563.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6b95daab0be455cd3ef",
+    "url": "/calculator/static/js/main.589d2d93.chunk.js"
+  },
+  {
+    "revision": "548babfea1b067596145",
+    "url": "/calculator/static/js/runtime-main.92c42aea.js"
+  }
+]);
